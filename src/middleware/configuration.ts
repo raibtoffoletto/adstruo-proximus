8783861c511b818829490a5955d7291f58697f7b
Raí B. Toffoletto <rai@toffoletto.me>
2025-04-14 16:33:54 +0100

    extend matcher

--- src/middleware/configuration.ts
+++ src/middleware/configuration.ts
@@ -1,6 +1,6 @@
 const config = {
   matcher: [
-    '/((?!_next/static|_next/image|locales|favicon.ico|manifest.json|robots.txt|sw.js|.*\\.png$).*)',
+    '/((?!_next/static|_next/image|locales|favicon.ico|manifest.json|robots.txt|sw.js|.*\\.png$|.*\\.css$).*)',
   ],
 };
 
